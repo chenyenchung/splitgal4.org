4807abefd6c7a3a9986de81badedb0e151e2e50e
Yen-Chung Chen <yenchung.chen@nyu.edu>
2023-07-04 23:39:04 -0400

    Clean up unused code

--- splitgal4_lines/views.py
+++ splitgal4_lines/views.py
@@ -1,13 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.template import loader
 from .models import fly_line
-
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
 
 def index(request):
     line_list = fly_line.objects.filter(internal_sharing=False).order_by("id")
@@ -15,5 +8,4 @@ def index(request):
     context = {
         "line_list": line_list,
     }
-    # output = ", ".join([q.gene_name for q in line_list])
     return HttpResponse(template.render(context, request))
