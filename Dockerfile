73f8c36a5c68d041585fc3018d80ef4076c61e5b
Yen-Chung Chen <chenyenjung@gmail.com>
2025-07-07 21:20:33 +0000

    [Admin] Misc
    - Mount the database with docker-compose.yml for persistence.
    - Improve the backup script so we don't hardcode the image.
    - Update the footer. It is July already after all...

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM python:3
 RUN pip3 install --upgrade pip
 RUN pip3 install django openpyxl six gunicorn requests django-turnstile
-VOLUME /home/splitgal4_org
 COPY . /home/splitgal4_org
 WORKDIR /home/splitgal4_org
-RUN python manage.py collectstatic
+RUN python manage.py collectstatic --noinput
 CMD ["gunicorn", "--bind" , "0.0.0.0:8000", "splitgal4db.wsgi"]
--- backup_db.sh
+++ backup_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-IMAGE=2b4b84e2cf3b
 TIMESTAMP=$(date +%Y%m%d)
-WORKDIR='/home/splitgal4_org'
 BKUPPATH='/home/chenyenjung/splitgal4_org/backup'
+SITEPATH='/home/chenyenjung/splitgal4_org/splitgal4.org/'
 
-sudo docker exec ${IMAGE} bash -c 'python manage.py dumpdata --indent 2 | gzip > bkup.json.gz'
-sudo docker cp ${IMAGE}:${WORKDIR}/bkup.json.gz ${BKUPPATH}/${TIMESTAMP}.json.gz
-sudo docker exec ${IMAGE} bash -c 'rm bkup.json.gz'
+cd ${SITEPATH}
+sudo docker compose exec web python manage.py dumpdata --indent 2 | gzip > ${BKUPPATH}/${TIMESTAMP}.json.gz
+# Also backup the actual sqlite file (belt and suspenders)
+cp db.sqlite3 ${BKUPPATH}/db_${TIMESTAMP}.sqlite3
